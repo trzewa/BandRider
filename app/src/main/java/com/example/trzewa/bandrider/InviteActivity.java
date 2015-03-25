d00aed53f8c67afade82d6dc7c92751526b14ccc
?pawel26 <pjablonski10@gmail.com>
2015-03-25 23:26:31 +0100

    dodanie aktywnosci logowania rejestracji ekranu powitalnego itp

---
+++ app/src/main/java/com/example/trzewa/bandrider/InviteActivity.java
@@ -0,0 +1,56 @@
+package com.example.trzewa.bandrider;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class InviteActivity extends Activity {
+
+    public static final int SPLASH_TIMEOUT = 2000;
+    private Timer timer = new Timer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       timer.schedule(new TimerTask() {
+           @Override
+           public void run() {
+               startActivity(new Intent(InviteActivity.this, LoginActivity.class));
+               finish();
+           }
+       }, SPLASH_TIMEOUT);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
