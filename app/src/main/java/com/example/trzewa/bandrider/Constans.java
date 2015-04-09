2ed7e57e6ec8240175b4a322dcf0e2092bce021a
trzewa <trzewa@wp.pl>
2015-04-10 01:02:11 +0200

    klasa Constans

---
+++ app/src/main/java/com/example/trzewa/bandrider/Constans.java
@@ -0,0 +1,8 @@
+package com.example.trzewa.bandrider;
+
+
+public class Constans {
+    public static final String NICKNAME_PARSE_KEY = "nickname";
+    public static final String USER_PARSE_KEY = "user";
+    public static final String ADDRESS_PARSE_KEY = "address";
+}
