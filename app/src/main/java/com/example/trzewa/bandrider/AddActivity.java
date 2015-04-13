fc64ad0316f640fc00e464dc97ab506d53b4c59b
?pawel26 <pjablonski10@gmail.com>
2015-04-13 19:02:39 +0200

    poprawa radio buttonów w addActivity dodanie pól w addstuffactivity

--- app/src/main/java/com/example/trzewa/bandrider/AddActivity.java
+++ app/src/main/java/com/example/trzewa/bandrider/AddActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
 public class AddActivity extends ActionBarActivity {
@@ -37,10 +38,13 @@ public class AddActivity extends ActionBarActivity {
                 int selectedId = radioChoiceGroup.getCheckedRadioButtonId();
 
                 radioItemButton = (RadioButton) findViewById(selectedId);
+                String test = (String) radioItemButton.getText();
+                Toast.makeText(AddActivity.this, test, Toast.LENGTH_SHORT).show();
+
 
                 if (radioItemButton.getText() =="Instrument")
                 {
-                    startActivity(new Intent(AddActivity.this, AddInstrumemtActivity.class));
+                    startActivity(new Intent(AddActivity.this, AddStuffActivity.class));
                 }
                 else
                 {
--- app/src/main/java/com/example/trzewa/bandrider/AddStuffActivity.java
+++ app/src/main/java/com/example/trzewa/bandrider/AddStuffActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class AddStuffActivity extends ActionBarActivity {
@@ -12,6 +17,48 @@ public class AddStuffActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_stuff);
+        String[] elementy = {"strunowe", "dęte", "perkusyjne"};
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_instrumemt);
+
+        final Spinner spinner = (Spinner)findViewById(R.id.spinner1);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, elementy);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int id, long position) {
+
+                Toast.makeText(AddStuffActivity.this, "Wybrano opcję" + (id + 1), Toast.LENGTH_SHORT).show();
+
+                switch((int)position)
+                {
+                    case 0:
+                        //wybrano pierwszy element
+                        break;
+                    case 1:
+                        //wybrano drugi element
+                        break;
+                    case 2:
+                        //wybrano trzeci element
+                        break;
+                    case 3:
+                        //wybrano czwarty element
+                        break;
+                    case 4:
+                        //wybrano piąty element
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
     }
 
 
