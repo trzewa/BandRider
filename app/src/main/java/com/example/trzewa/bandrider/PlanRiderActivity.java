1177a5fc742fdb7dc3a7abc66b1938e68bce1b3d
?pawel26 <pjablonski10@gmail.com>
2015-04-27 19:46:33 +0200

    sprawdzanie połączenia z Internetem - POCZĄTEK RIDERA

--- app/src/main/java/com/example/trzewa/bandrider/PlanRiderActivity.java
+++ app/src/main/java/com/example/trzewa/bandrider/PlanRiderActivity.java
@@ -1,17 +1,48 @@
 package com.example.trzewa.bandrider;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.parse.ParseObject;
 
 
 public class PlanRiderActivity extends ActionBarActivity {
-
+    private AlertDialog alertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_rider);
+        final Context context = getApplicationContext();
+        boolean networkstate = Utilities.getConnectivityStatus(context);
+
+        if (networkstate == true)
+        {
+
+            Toast.makeText(getApplicationContext(), "nawiazano połączenie", Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            alertDialog = new AlertDialog.Builder(PlanRiderActivity.this)
+                    .setTitle("Brak Internetu")
+                    .setMessage("brak połączenia z siecią Internet")
+                    .setNegativeButton("Ok", new AlertDialog.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            alertDialog.cancel();
+                        }
+                    })
+                    .create();
+            alertDialog.show();
+
+
+        }
+
     }
 
 
