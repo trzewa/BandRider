2708cf70ab4a9eddb9e3ebe90ffa33406742d0ab
?pawel26 <pjablonski10@gmail.com>
2015-05-03 19:14:06 +0200

    PlanRiderActivity porządki vol. 2

--- app/src/main/java/com/example/trzewa/bandrider/PlanRiderActivity.java
+++ app/src/main/java/com/example/trzewa/bandrider/PlanRiderActivity.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -53,24 +52,6 @@ public class PlanRiderActivity extends Activity  {
 
             Toast.makeText(getApplicationContext(), "nawiazano połączenie", Toast.LENGTH_LONG).show();
 
-            //Toast.makeText( mContext,"Select aaaa "+choiceList[selected],Toast.LENGTH_SHORT).show();
-            /*
-
-
-            tutaj obsługa listy - zmienna selected zwraca indeks z tablicy choiceList
-            zależnie co zostało zwrócone możemy zrobić tu obsługę dla odpowiedniej listy np.
-
-            if selected.equals("Instrumenty") // albo może lepiej case
-            {
-              ładujemy widok dla listy instrumentów
-              tworzymy adapter
-              na liście głównej najlepiej jakby elementy wyglądały tak samo np.
-              nazwa instrumentu właściciel ocena
-              dla sprzętu
-              nazwa instrumentu właściciel ocena
-              a np różne dla item_selected
-            }
-             */
             switch(choiceList[selected]) {
                 case "Instrumenty":
                     setContentView(R.layout.activity_plan_rider);
@@ -93,12 +74,6 @@ public class PlanRiderActivity extends Activity  {
 
             }
 
-
-
-
-
-
-
         } else {
             alertDialog = new AlertDialog.Builder(PlanRiderActivity.this)
                     .setTitle("Brak Internetu")
@@ -115,148 +90,7 @@ public class PlanRiderActivity extends Activity  {
 
 
         }
-        /*final Spinner spinner = (Spinner) findViewById(R.id.spinnerCategory);
 
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, Constans.ELEMENTY);
-
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-
-            @Override
-            public void onItemSelected(AdapterView<?> arg0, View arg1,
-                                       int id, long position) {
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                String test = currentUser.getUsername();
-                Toast.makeText(PlanRiderActivity.this, "Wybrano opcję " + (id + 1), Toast.LENGTH_SHORT).show();
-                Toast.makeText(PlanRiderActivity.this, "Wybrano opcję " + (test), Toast.LENGTH_SHORT).show();
-                String category = null;
-
-                switch ((int) position) {
-                    case 0:
-                        category = "strunowe";
-
-                        Toast.makeText(PlanRiderActivity.this, category, Toast.LENGTH_SHORT).show();
-                        listaZmiana(category);
-                        break;
-                    case 1:
-                        category = "dęte";
-                        Toast.makeText(PlanRiderActivity.this, category, Toast.LENGTH_SHORT).show();
-                        listaZmiana(category);
-                        break;
-                    case 2:
-                        category = "perkusyjne";
-
-                        Toast.makeText(PlanRiderActivity.this, category, Toast.LENGTH_SHORT).show();
-
-                        break;
-                    case 3:
-
-                        break;
-                    case 4:
-                        //wybrano piąty element
-                        break;
-                }
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-
-            }
-        });*/
-
-
-
-
-
-    }
-
-    private int selected = 0;
-    private int buffKey = 0; // add buffer value
-    private int showDialogButtonClick() {
-        Log.i(TAG, "show Dialog ButtonClick");
-        AlertDialog.Builder builder =
-                new AlertDialog.Builder(mContext);
-        builder.setTitle("Wybierz kategorię");
-
-
-
-        builder.setSingleChoiceItems(
-                choiceList,
-                selected,
-                new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(
-                            DialogInterface dialog,
-                            int which) {
-                        //set to buffKey instead of selected
-                        //(when cancel not save to selected)
-                        buffKey = which;
-                    }
-                })
-                .setCancelable(false)
-                .setPositiveButton("Ok",
-                        new DialogInterface.OnClickListener()
-                        {
-                            @Override
-                            public void onClick(DialogInterface dialog,
-                                                int which) {
-                                Log.d(TAG,"Which value="+which);
-                                Log.d(TAG,"Selected value="+buffKey);
-
-                                //set buff to selected
-                                selected = buffKey;
-                               // listView.setVisibility(View.VISIBLE);
-
-                            }
-                        }
-
-                )
-                .setNegativeButton("Anuluj",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog,
-                                                int which) {
-
-                            }
-                        }
-                );
-
-        AlertDialog alert = builder.create();
-        alert.show();
-        return selected;
-    }
-
-
-
-    public void listaZmiana (String category) {
-        Toast.makeText(PlanRiderActivity.this, category + " OKI", Toast.LENGTH_SHORT).show();
-        /*ParseQuery<ParseObject> query = ParseQuery.getQuery(category);
-        query.whereEqualTo("category", category);
-
-
-        query.findInBackground(new FindCallback<ParseObject>()
-
-        {
-            public void done(List<ParseObject> scoreList, ParseException e) {
-                if (e == null) {
-                    Log.d("name", "Retrieved " + scoreList.size() + " scores");
-
-                } else {
-                    Log.d("name", "Error: " + e.getMessage());
-                }
-            }
-
-            @Override
-            public void done(List<ParseObject> parseObjects, com.parse.ParseException e) {
-
-            }
-
-        })
-*/
    ; }
 
     @Override
